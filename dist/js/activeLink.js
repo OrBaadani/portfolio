783c8c289995d25e6f35f3c8dada033d802ba34e
Or Baadani <orbaadani@gmail.com>
2022-02-07 17:48:28 +0200

    scroll fix

--- dist/js/activeLink.js
+++ dist/js/activeLink.js
@@ -1,11 +1,20 @@
 var currentLink = null;
 function changeLinkColor(link, scroll) {
-    console.log('', scroll);
-    window.scrollTo({
-        top: document.querySelector(`.${scroll}`).offsetTop - 70,
-        // left: 100,
-        behavior: 'smooth'
-    });
+    console.log('', window.innerWidth);
+    if (window.innerWidth < 650) {
+        window.scrollTo({
+            top: document.querySelector(`.${scroll}`).offsetTop - 120,
+            // left: 100,
+            behavior: 'smooth'
+        })
+    } else {
+        window.scrollTo({
+            top: document.querySelector(`.${scroll}`).offsetTop - 70,
+            // left: 100,
+            behavior: 'smooth'
+        });
+    }
+
     if (currentLink !== null) {
         currentLink.style.borderColor = link.style.borderColor;
     }
@@ -17,12 +26,21 @@ function changeLinkColor(link, scroll) {
 window.onscroll = function () { myFunction() };
 function myFunction() {
     var header = document.querySelector(".header-home");
-    if ((window.pageYOffset) > header.offsetHeight - 71) {
-        document.querySelector('.header-inner').classList.add("sub-header");
-
-
+    if (window.innerWidth < 650) {
+        if ((window.pageYOffset) > header.offsetHeight - 121) {
+            document.querySelector('.header-inner').classList.add("sub-header");
+        }
+        if (window.pageYOffset < header.offsetHeight - 121) {
+            document.querySelector('.header-inner').classList.remove("sub-header");
+        }
     }
-    if (window.pageYOffset < header.offsetHeight - 71) {
-        document.querySelector('.header-inner').classList.remove("sub-header");
+    else {
+        if ((window.pageYOffset) > header.offsetHeight - 71) {
+            document.querySelector('.header-inner').classList.add("sub-header");
+        }
+        if (window.pageYOffset < header.offsetHeight - 71) {
+            document.querySelector('.header-inner').classList.remove("sub-header");
+        }
     }
+
 }
